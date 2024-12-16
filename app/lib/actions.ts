eaeaaf5ef3a78758f4fef3033f8552cb8e4b4ea0
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2024-12-16 19:52:23 +0100

    Cap. 12 - Inserting Data into the DB, Revalidating and Redirecting

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,9 @@
 'use server';
 
 import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -23,4 +26,11 @@ export async function createInvoice(formData: FormData) {
 
     const date = new Date().toISOString().split('T')[0];
 
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
 }
