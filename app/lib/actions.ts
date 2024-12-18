89141692ee71fb5110fac49ff7d52d8a7fa504dd
Eva Gandia Belda <147039955+evagandiabelda@users.noreply.github.com>
2024-12-18 19:19:42 +0100

    Cap. 13 - Handling Errors with 'error.tsx' and 'not-found.tsx'.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -67,6 +67,9 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+  // Simular un error para probar:
+  throw new Error('Failed to Delete Invoice');
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
